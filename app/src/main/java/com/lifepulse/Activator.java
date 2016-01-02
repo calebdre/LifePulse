38c24fc673e2ce25fea5d2cbfed0f2e2b24a8557
Caleb Lewis <caleb_dre@yahoo.com>
2016-01-02 14:47:57 -0800

    added auth for digital life

---
+++ app/src/main/java/com/lifepulse/Activator.java
@@ -0,0 +1,60 @@
+package com.lifepulse;
+
+import android.support.annotation.Nullable;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import de.greenrobot.event.EventBus;
+
+public class Activator {
+
+    public void authenticate(){
+        Request.Builder builder = new Request.Builder();
+        HashMap<String, String> params = new HashMap<>();
+        params.put("userId", "553474421");
+        params.put("password", "NO-PASSWD");
+        params.put("appKey", "BE_75FCFB017D1EEF50_1");
+
+        builder.url("https://systest.digitallife.att.com/penguin/api/authtokens");
+        builder.post(transformToRequestBody(params));
+        final Request request = builder.build();
+        OkHttpClient client = new OkHttpClient();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {}
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                JsonElement json = new JsonParser().parse(response.body().string());
+                String authToken = json.getAsJsonObject().get("content").getAsJsonObject().get("authToken").getAsString();
+                String requestToken = json.getAsJsonObject().get("content").getAsJsonObject().get("requestToken").getAsString();
+                String gateway = json.getAsJsonObject().get("content").getAsJsonObject().get("gateways").getAsJsonArray().get(0).getAsJsonObject().get("id").getAsString();
+                AuthToken token = new AuthToken(authToken, requestToken, gateway);
+                EventBus.getDefault().post(new AuthenticatedEvent(token));
+            }
+        });
+
+    }
+
+    @Nullable
+    private RequestBody transformToRequestBody(HashMap<String, String> query) {
+        if(query == null) return null;
+
+        FormEncodingBuilder formBody = new FormEncodingBuilder();
+        for (Map.Entry<String, String> entry : query.entrySet()) {
+            formBody.add(entry.getKey(), entry.getValue());
+        }
+        return formBody.build();
+    }
+}
--- app/src/main/java/com/lifepulse/MainActivity.java
+++ app/src/main/java/com/lifepulse/MainActivity.java
@@ -3,11 +3,30 @@ package com.lifepulse;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import de.greenrobot.event.EventBus;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    public void onEvent(AuthenticatedEvent e){
+        AuthToken token = e.getToken();
+
     }
 }
